86308b863c51923ffac66f99cb99407bccd35b53
Nyaga <nyaga@MBP-von-Nyaga.fritz.box>
2024-11-19 12:53:57 +0100

    Day one

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Hello, programmer! Input your first name\n";
+    std::string first_name;
+    std::cin >> first_name;
+    std::cout << "Welcome, " << first_name << "! \n";
+
+};
+
+
+
+
+
